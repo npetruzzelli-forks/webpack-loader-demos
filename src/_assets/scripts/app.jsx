7d0361e6006f216405463a6b997663bbd212620b
Nick Petruzzelli <code.npetruzzelli@gmail.com>
2018-12-10 10:56:44 -0500

    Initial Source Materials

---
+++ src/_assets/scripts/app.jsx
@@ -0,0 +1,60 @@
+// #============================================================================
+// # IMPORT NODE MODULES
+// #
+import React, { Component, Fragment } from 'react'
+
+// #============================================================================
+// # CODE
+// #
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Header</h1>
+        <ul>
+          <li>
+            <a href="/src">/src</a> - source code
+          </li>
+          <li>
+            <a href="/webpack-dev-server">/webpack-dev-server</a> - bundled code
+          </li>
+        </ul>
+        <p className="normally-transparent">
+          <span className="hide-selection text-success text-weight-bold">
+            Bootstrap styles in use!
+          </span>
+        </p>
+        <p className="my-lead-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          <span className="my-image" />
+        </p>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </p>
+      </Fragment>
+    )
+  }
+}
+
+module.exports = App
---
+++ src/index.css.jsx
@@ -0,0 +1,18 @@
+// #============================================================================
+// # IMPORT NODE MODULES
+// #
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// #============================================================================
+// # IMPORT LOCAL MODULES
+// #
+import App from './_assets/scripts/app'
+import './_assets/stylesheets/styles.css'
+
+// #============================================================================
+// # CODE
+// #
+
+console.log('Starting App')
+ReactDOM.render(<App />, document.getElementById('root'))
